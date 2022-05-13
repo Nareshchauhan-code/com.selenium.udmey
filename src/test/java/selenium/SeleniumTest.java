f46a36db7e1cc89b67dfe379c361c1276c48bf2b
Naresh Chauhan <62231215+Nareshchauhan-code@users.noreply.github.com>
2022-05-13 15:50:06 +0530

    Initial commit

---
+++ src/test/java/selenium/SeleniumTest.java
@@ -0,0 +1,23 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SeleniumTest {
+
+    @Test
+    void getLaunchUrl(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://google.com");
+        driver.quit();
+
+    }
+}
